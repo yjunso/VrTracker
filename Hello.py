e7c20be0934d455409c06d79a10bdfcba48a36ed
yjunso <yjun.so@gmail.com>
2016-09-13 16:53:19 +0900

    update hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,20 @@
-Hello
+from pixy import *
+import sys
+
+def main():
+	pixy_init()
+	get_pixy()
+
+if __name__ == "__main__":
+	main()
+	sys.exit(0)
+
+def get_pixy():
+	blocks = BlockArray(100)
+	frame = 0
+
+	while 1 :
+		count = pixy_get_blocks(100, blocks)
+		if count > 0 :
+	  		print 'count = %d' % count
+	  

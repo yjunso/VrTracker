a1c5289a72cc75185c0d1e69e554961381bc478f
yjunso <yjun.so@gmail.com>
2016-09-13 17:02:39 +0900

    update hello.py

--- Hello.py
+++ Hello.py
@@ -5,9 +5,7 @@ def main():
 	pixy_init()
 	get_pixy()
 
-if __name__ == "__main__":
-	main()
-	sys.exit(0)
+
 
 def get_pixy():
 	blocks = BlockArray(100)
@@ -17,4 +15,7 @@ def get_pixy():
 		count = pixy_get_blocks(100, blocks)
 		if count > 0 :
 	  		print 'count = %d' % count
-	  
+
+if __name__ == "__main__":
+	main()
+	sys.exit(0)
